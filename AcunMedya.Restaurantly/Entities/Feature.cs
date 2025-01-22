27bfb2e2bee16205387a0c8823ff7a467d552aa9
busenurdmb <bus.dmb@gmail.com>
2025-01-22 20:42:45 +0300

    Add project files.

---
+++ AcunMedya.Restaurantly/Entities/Feature.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AcunMedya.Restaurantly.Entities
+{
+    public class Feature
+    {
+        public int FeatureId { get; set; }
+        public string SubTitle { get; set; }
+        public string Title { get; set; }
+        public string VideoUrl { get; set; }
+        public string İmageUrl { get; set; }
+    }
+}
