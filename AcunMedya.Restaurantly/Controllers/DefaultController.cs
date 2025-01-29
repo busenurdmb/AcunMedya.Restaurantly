fda91b00707d3640a63f6cb251bc26d8351bf84c
busenurdmb <bus.dmb@gmail.com>
2025-01-29 20:38:46 +0300

    partialhead partialtop partialnavbar

--- AcunMedya.Restaurantly/Controllers/DefaultController.cs
+++ AcunMedya.Restaurantly/Controllers/DefaultController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AcunMedya.Restaurantly.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,27 @@ namespace AcunMedya.Restaurantly.Controllers
 {
     public class DefaultController : Controller
     {
+        RestaurantlyContext Db = new RestaurantlyContext();
         // GET: Default
         public ActionResult Index()
         {
             return View();
+        }
+
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialTop()
+        {
+            ViewBag.Call = Db.Adresss.Select(x => x.Call).FirstOrDefault();
+            ViewBag.OpenHours = Db.Adresss.Select(x => x.OpenHours).FirstOrDefault();
+            return PartialView();
+        }
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
         }
     }
 }
